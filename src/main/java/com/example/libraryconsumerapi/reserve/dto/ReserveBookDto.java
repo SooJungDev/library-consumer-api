14b0a9c526c37972c76548e67bfef95c021dbd56
crystal <crystal2840@gmail.com>
2024-01-28 11:27:54 +0900

    feat: topic 수신 관련 변경 및 보안으로 인해 설정 제거..

--- src/main/java/com/example/libraryconsumerapi/reserve/dto/ReserveBookDto.java
+++ src/main/java/com/example/libraryconsumerapi/reserve/dto/ReserveBookDto.java
@@ -1,14 +1,20 @@
 package com.example.libraryconsumerapi.reserve.dto;
 
+import java.time.ZonedDateTime;
+
 import lombok.Data;
 
 @Data
 public class ReserveBookDto {
-    private Long id;
+    private Long bookId;
 
     private String title;
 
     private int quantity;
+
+    private int totalStockQuantity;
+
+    private ZonedDateTime orderAt;
 
     public String getSlackTitle() {
         return String.format("%s 책이 주문되었습니다.", title);
